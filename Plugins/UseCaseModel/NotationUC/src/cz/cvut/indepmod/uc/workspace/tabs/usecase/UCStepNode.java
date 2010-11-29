8721b6310d9813250c1f49fa907cd02caecd257c
Viktor Bohdal <viktor.bohdal@gmail.com>
2010-11-29 13:13:17 +0100

    Zapomenute soubory v commitu.

---
+++ Plugins/UseCaseModel/NotationUC/src/cz/cvut/indepmod/uc/workspace/tabs/usecase/UCStepNode.java
@@ -0,0 +1,16 @@
+package cz.cvut.indepmod.uc.workspace.tabs.usecase;
+
+import javax.swing.tree.DefaultMutableTreeNode;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Viktor
+ * Date: 24.11.2010
+ * Time: 11:57:06
+ * To change this template use File | Settings | File Templates.
+ */
+public class UCStepNode extends DefaultMutableTreeNode {
+    public UCStepNode(String str) {
+        super(str);
+    }
+}
